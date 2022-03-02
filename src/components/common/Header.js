fcb66800f1b131297c877ce8c5855c30d81f07b3
Ademola <a.g.jegede@wlv.ac.uk>
2022-03-02 03:37:38 +0000

    new commit 02/03/22

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -15,7 +15,6 @@ background-image:linear-gradient(to right, #E5E5E5,  #E5E5E5);
 border-bottom: 3px solid  #F47806;
 background-color: #FFFF;
 background-image: url("https://www.transparenttextures.com/patterns/cubes.png");
-/* This is mostly intended for prototyping; please download the pattern and re-host for production environments. Thank you! */
 
 h2{
     color: #F47806;
